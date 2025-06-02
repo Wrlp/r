41fbe07a97dce95923a2a5fc6a2c2cb2bdd9c4f6
PouLoulou18 <louis.robillard18@gmail.com>
2025-06-02 16:03:18 +0200

    Update partie 2

--- projet.py
+++ projet.py
@@ -10,6 +10,9 @@ points = [(1,1), (1,2), (1,5), (3,4), (4,3), (6,2), (0,4)]
 x = np.array([p[0] for p in points])
 y = np.array([p[1] for p in points])
 
+
+# 2.1 Calcul des Coefficients de Régression
+
 # Moyennes
 x_moy = np.mean(x)
 y_moy = np.mean(y)
@@ -18,13 +21,20 @@ y_moy = np.mean(y)
 b1 = np.sum((x - x_moy) * (y - y_moy)) / np.sum((x - x_moy)**2)
 b0 = y_moy - b1 * x_moy
 
-# Prédictions
+# Modèle de prédictions
 y_estime = b0 + b1 * x
+
+
+# 2.3 Coefficient de Détermination R²
 
 # R²
 SCE = np.sum((y - y_estime) ** 2)
 SCT = np.sum((y - y_moy) ** 2)
+SCR = np.sum((y_estime - y_moy) ** 2)
 R2 = 1 - SCE / SCT
+
+
+# 2.2 Visualisation de la Droite de Régression
 
 # Affichage
 plt.scatter(x, y, label='Points')
@@ -35,3 +45,8 @@ plt.xlabel("x")
 plt.ylabel("y")
 plt.grid(True)
 plt.show()
+plt.savefig("figure_2_1.jpg")
+print("Graphique enregistré dans le fichier figure_2_1.jpg")
+
+
+
