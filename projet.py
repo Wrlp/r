2de06add33cd405c98370bec83788d771e92a76b
marieept <marie.epinat@isen-ouest.yncrea.fr>
2025-06-02 14:20:12 +0200

    afficher_stats

--- projet.py
+++ projet.py
@@ -1,2 +1,9 @@
+import numpy as np
+
 points =[(1,1),(1,2),(1,5),(3,4),(4,3),(6,2),(0,4)]
 
+def afficher_stats(valeurs):
+    print("Moyenne :", np.mean(valeurs))
+    print("Médiane :", np.median(valeurs))
+    print("Variance :", np.var(valeurs))
+    prnt("")
