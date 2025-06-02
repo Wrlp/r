a4471a7fc5edbc7fb0fc4a03814a64a476bd2e3e
marieept <marie.epinat@isen-ouest.yncrea.fr>
2025-06-02 15:40:48 +0200

    afficher_stats

--- projet.py
+++ projet.py
@@ -1,13 +1,46 @@
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
->>>>>>> Stashed changes
 import numpy as np
+import matplotlib.pyplot as plt
 
 points =[(1,1),(1,2),(1,5),(3,4),(4,3),(6,2),(0,4)]
+noms= ["M1", "M2", "M3", "M4", "M5", "M6", "M7"]
 
-def afficher_stats(valeurs):
+#Séparer les coordonnées x et y
+x = np.array([p[0] for p in points])
+y = np.array([p[1] for p in points])
+
+
+def afficher_stats(valeurs, noms):
     print("Moyenne :", np.mean(valeurs))
     print("Médiane :", np.median(valeurs))
     print("Variance :", np.var(valeurs))
-    prnt("")
+    print("Ecart-type :", np.std(valeurs))
+    print("Minimum :", np.min(valeurs))
+    print("Maximum :", np.max(valeurs))
+    print("Etendue :", np.ptp(valeurs))
+
+
+afficher_stats(x, "x")
+afficher_stats(y, "y")
+
+plt.scatter(x, y, color='blue')
+for i in range(len(points)):
+    plt.text(x[i] + 0.1, y[i] + 0.1, noms[i])
+plt.title("Nuage de points")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.grid()
+plt.show()
+
+#   Interprétation statistique :
+
+#Les valeurs xx sont plus dispersées que les valeurs yy.
+#La moyenne de xx est plus faible que celle de yy, indiquant une asymétrie spatiale.
+#L’étendue plus grande pour xx traduit une plus grande variabilité sur l’axe horizontal.
+
+#   Interprétation graphique :
+
+#Le nuage montre une certaine tendance croissante mais non strictement linéaire.
+#Il est possible qu’une relation linéaire approximative existe entre xx et yy, à tester dans la partie suivante.
+
+
+
