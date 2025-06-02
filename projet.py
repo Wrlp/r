2c0a9e12a49cf172120682a1fa9ccce77a3bcdc1
PouLoulou18 <louis.robillard18@gmail.com>
2025-06-02 17:22:38 +0200

    Update

--- projet.py
+++ projet.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats
+import math
+import pandas as pd
 
 points =[(1,1),(1,2),(1,5),(3,4),(4,3),(6,2),(0,4)]
 noms= ["M1", "M2", "M3", "M4", "M5", "M6", "M7"]
@@ -215,3 +217,90 @@ print("Intervalle de confiance à 95% pour b0:", IC_b0)
 
 # 4.6 Interprétation des Tests Statistiques
 
+
+#########################################
+#               Partie 5                #
+#########################################
+
+# Classification Ascendante Hiérarchique (CAH)
+
+# 1.a
+def dist(p1, p2):
+    """Distance euclidienne"""
+    distance = math.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)
+    print("La distance euclidienne vaut : ", distance)
+    return distance
+
+# 1.b
+def dist1(p1, p2):
+    """Distance de Manhattan"""
+    distance = abs(p1[0]-p2[0]) + abs(p1[1]-p2[1])
+    print("La distance de Manhattan vaut : ", distance)
+    return distance
+
+# 1.c
+def dist_inf(p1, p2):
+    """Distance de Chebyshev (max)"""
+    distance = max(abs(p1[0]-p2[0]), abs(p1[1]-p2[1]))
+    print("La distance de Chebyshev vaut : ", distance)
+    return distance
+
+# 1.d : Distance de Ward ???
+
+
+# 2.
+def dist_min(tableau, dist_func):
+    min_d = float('inf')
+    couple_points = None
+    for i in range(len(tableau)):
+        for j in range(i+1, len(tableau)):
+            d = dist_func(tableau[i], tableau[j])
+            if d < min_d:
+                min_d = d
+                couple_points = (tableau[i], tableau[j])
+    return couple_points, min_d
+
+
+# 3.
+
+# Tracé
+plt.scatter(x, y, color='blue')
+for i in range(len(points)):
+    plt.text(x[i] + 0.1, y[i] + 0.1, noms[i])
+plt.title("Nuage de points")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.grid()
+plt.show()
+
+# Initialisation de la matrice
+n = len(x)
+matrice_1 = np.zeros((n, n))
+
+# Remplissage de la matrice avec d²
+for i in range(n):
+    for j in range(n):
+        xi, yi = points[x[i]]
+        xj, yj = points[x[j]]
+        d_squared = (xi - xj)**2 + (yi - yj)**2
+        matrice_1[i][j] = d_squared
+
+# Affichage avec pandas pour lisibilité
+data = pd.DataFrame(matrice_1, index=x, columns=x)
+print("Matrice des distances euclidiennes au carré :\n")
+print(data.round(1))
+
+
+# Encadrer M1 et M7 (Classe Γ₁)
+x_vals = [points["M1"][0], points["M7"][0]]
+y_vals = [points["M1"][1], points["M7"][1]]
+plt.plot(x_vals, y_vals, 'ro--')
+plt.scatter(x_vals, y_vals, color='red')
+plt.title("Regroupement initial : Classe Γ₁ = {M1, M7}")
+plt.grid(True)
+plt.xlim(-1, 7)
+plt.ylim(0, 6)
+plt.show()
+
+plt.savefig("figure_5_3.jpg")
+print("Graphique enregistré dans le fichier figure_5_3.jpg")
