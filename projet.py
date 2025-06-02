76deff15e427aeae1c614993305a04ed9201e0de
PouLoulou18 <louis.robillard18@gmail.com>
2025-06-02 16:49:15 +0200

    update partie 5

--- projet.py
+++ projet.py
@@ -50,6 +50,64 @@ print("Graphique enregistré dans le fichier figure_2_1.jpg")
 
 
 #########################################
-#               Partie 4                #
+#               Partie 5                #
 #########################################
 
+# Classification Ascendante Hiérarchique (CAH)
+
+import math
+
+# 1.a
+def dist(p1, p2):
+    """Distance euclidienne"""
+    distance = math.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)
+    print("La distance euclidienne vaut : ", distance)
+    return distance
+
+# 1.b
+def dist1(p1, p2):
+    """Distance de Manhattan"""
+    distance = abs(p1[0]-p2[0]) + abs(p1[1]-p2[1])
+    print("La distance de Manhattan vaut : ", distance)
+    return distance
+
+# 1.c
+def dist_inf(p1, p2):
+    """Distance de Chebyshev (max)"""
+    distance = max(abs(p1[0]-p2[0]), abs(p1[1]-p2[1]))
+    print("La distance de Chebyshev vaut : ", distance)
+    return distance
+
+# 1.d : Distance de Ward ???
+
+
+# 2.
+def dist_min(tableau, dist_func):
+    min_d = float('inf')
+    couple_points = None
+    for i in range(len(tableau)):
+        for j in range(i+1, len(tableau)):
+            d = dist_func(tableau[i], tableau[j])
+            if d < min_d:
+                min_d = d
+                couple_points = (tableau[i], tableau[j])
+    return couple_points, min_d
+
+
+# 3.
+# Tracé
+plt.figure(figsize=(6, 6))
+for name, (x, y) in points.items():
+    plt.scatter(x, y, color='blue')
+    plt.text(x + 0.1, y, name, fontsize=12)
+
+# Encadrer M3 et M7 (Classe Γ₁)
+x_vals = [points["M3"][0], points["M7"][0]]
+y_vals = [points["M3"][1], points["M7"][1]]
+plt.plot(x_vals, y_vals, 'ro--')
+plt.scatter(x_vals, y_vals, color='red')
+plt.title("Regroupement initial : Classe Γ₁ = {M3, M7}")
+plt.grid(True)
+plt.xlim(-1, 7)
+plt.ylim(0, 6)
+plt.show()
